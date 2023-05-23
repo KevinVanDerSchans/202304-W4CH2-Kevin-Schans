28bc639a45318b6fb6b00dd7781fadae943eed34
Kevin Schans <kevinvdsd@hotmail.com>
2023-05-23 23:30:11 +0200

    Change to string the gender values

--- src/models/personal-data.ts
+++ src/models/personal-data.ts
@@ -2,7 +2,7 @@ export type PersonalData = {
   name: string;
   lastName: string;
   birthDate: string;
-  gender: male | female | other | preferNotToMention;
+  gender: "male" | "female" | "other" | "preferNotToMention";
   email: string;
-  newsletter = boolean;
+  newsletter: boolean;
 };
